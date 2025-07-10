a40594b77a1f4238009836b262b324217b1c0052
Nicolás Tapié <nicoatapie@gmail.com>
2025-07-10 18:20:02 -0300

    refactor: update Glide.js initialization and event handling for improved functionality

--- src/utils/glide.js
+++ src/utils/glide.js
@@ -1,12 +1,24 @@
 import Glide from '@glidejs/glide';
 
 document.addEventListener('DOMContentLoaded', () => {
-  new Glide('.glide', {
+  const glide = new Glide('.glide', {
     type: 'carousel',
     startAt: 0,
     perView: 14,
     autoplay: 1000,
     animationDuration: 2000,
     animationTimingFunc: 'ease-in-out',
-  }).mount();
+  });
+
+  glide.mount();
+
+  const glideElement = document.querySelector('.glide');
+
+  glideElement.addEventListener('mouseenter', () => {
+    glide.pause();
+  });
+
+  glideElement.addEventListener('mouseleave', () => {
+    glide.play();
+  });
 });
