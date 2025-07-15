e7a8130983013df68879d13855f8ebff2c574e51
Nicolás Tapié <nicoatapie@gmail.com>
2025-07-15 20:37:05 -0300

    Add social media icons and create reusable SocialLinks component
    - Imported Instagram, Github, Linkedin, and SendEmail SVG icons in index.js.
    - Created a new SocialLinks.astro component for rendering social media links with customizable href and name props.

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import Instagram from '@/assets/Instagram.svg';
+import Github from '@/assets/Github.svg';
+import Linkedin from '@/assets/Linkedin.svg';
+import SendEmail from '@/assets/SendEmail.svg';
+
+export { Instagram, Github, Linkedin, SendEmail };
