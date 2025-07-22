c65ac32844f513e0558a081f889025407306641c
Nicolás Tapié <nicoatapie@gmail.com>
2025-07-22 19:07:52 -0300

    Refactor code structure for improved readability and maintainability

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,22 @@ import Instagram from '@/assets/Instagram.svg';
 import Github from '@/assets/Github.svg';
 import Linkedin from '@/assets/Linkedin.svg';
 import SendEmail from '@/assets/SendEmail.svg';
+import Email from '@/assets/Email.svg';
+import ArrowUp from '@/assets/ArrowUp.svg';
+import Profile from '@/assets/Profile.svg';
+import Code from '@/assets/Code.svg';
+import DownArrow from '@/assets/DownArrow.svg';
+import ArrowRight from '@/assets/ArrowRight.svg';
 
-export { Instagram, Github, Linkedin, SendEmail };
+export {
+  Instagram,
+  Github,
+  Linkedin,
+  SendEmail,
+  Email,
+  ArrowUp,
+  Profile,
+  Code,
+  DownArrow,
+  ArrowRight,
+};
